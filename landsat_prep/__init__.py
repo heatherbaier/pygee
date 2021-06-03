bf256f7fe1b119d215122e823b32866331de404a
heatherbaier <hmbaier@email.wm.edu>
2021-06-03 09:24:51 -0400

    downloadGB

--- landsat_prep/__init__.py
+++ landsat_prep/__init__.py
@@ -3,3 +3,4 @@ from .save_boxes import create_imagery_boxes, create_single_imagery_boxes
 from .download_imagery import download_imagery
 from .save_pngs import save_pngs, clip_image
 from .prep_landsat import prep_landsat
+from .downloadGB import downloadGB
