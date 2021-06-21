9957c3c002d0a84608342d09dac581edea1fe96d
heatherbaier <hmbaier@email.wm.edu>
2021-06-21 16:39:07 -0400

    fix

--- landsat_prep/__init__.py
+++ landsat_prep/__init__.py
@@ -1,5 +1,5 @@
 from .helpers import *
-from .save_boxes import create_imagery_boxes, create_tiles
+from .save_boxes import create_imagery_boxes, create_single_imagery_boxes
 from .download_imagery import download_imagery, download_boundary_imagery
 from .save_pngs import save_pngs, clip_image, save_boundary_pngs
 from .prep_landsat import prep_landsat
