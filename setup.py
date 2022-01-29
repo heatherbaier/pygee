aeb2357f8d6bafb16fdcb36e9431182a888d2acb
Heather Baier <44214106+heatherbaier@users.noreply.github.com>
2022-01-28 20:50:05 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='pygee',
     version='0.0.1',
     description='Landsat prep tools',
-    url='git@github.com:heatherbaier/landsat-imagery-prep.git',
+    url='git@github.com:heatherbaier/pygee.git',
     author='Heather Baier',
     author_email='hmbaier@email.wm.edu',
     license='unlicense',
