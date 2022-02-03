4376ac867bf6c29d36d28f4b835f2388efe71925
heatherbaier <hmbaier@email.wm.edu>
2022-02-03 18:11:24 -0500

    removing old functions

--- pygee/__init__.py
+++ pygee/__init__.py
@@ -2,6 +2,6 @@
 from .utils import *
 from .save_boxes import create_imagery_boxes, create_single_imagery_boxes
 from .download_imagery import download_imagery
-from .save_pngs import save_pngs, clip_image, save_boundary_pngs
+from .save_pngs import save_pngs
 from .prep_landsat import prep_landsat
 from .downloadGB import downloadGB
