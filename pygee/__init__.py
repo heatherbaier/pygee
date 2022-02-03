8b4496fe54e8d7f9b49c15d1d72dbbe0a2149f05
heatherbaier <hmbaier@email.wm.edu>
2022-02-03 18:09:58 -0500

    test utils final

--- pygee/__init__.py
+++ pygee/__init__.py
@@ -1,5 +1,5 @@
 # from .utils import *
-import .utils
+from .utils import *
 from .save_boxes import create_imagery_boxes, create_single_imagery_boxes
 from .download_imagery import download_imagery
 from .save_pngs import save_pngs, clip_image, save_boundary_pngs
