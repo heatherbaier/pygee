9389d1408314d077d4c314a79d55d14065f9c896
heatherbaier <hmbaier@email.wm.edu>
2022-02-03 18:13:09 -0500

    removing old functions

--- pygee/main.py
+++ pygee/main.py
@@ -8,7 +8,7 @@ import pyproj
 from .utils import *
 from .save_boxes import create_imagery_boxes, create_single_imagery_boxes
 from .download_imagery import download_imagery
-from .save_pngs import save_pngs, clip_image
+from .save_pngs import save_pngs
 from .downloadGB import downloadGB
 
 GB_PATH = "./data/MEX/MEX_ADM2_fixedInternalTopology.shp"
