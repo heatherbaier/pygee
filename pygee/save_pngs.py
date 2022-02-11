1ae0515c9d9fde7f74923905d08034e47946ab5a
Heather Baier <44214106+heatherbaier@users.noreply.github.com>
2022-02-11 14:00:58 -0500

    landsat 5 saving

--- pygee/save_pngs.py
+++ pygee/save_pngs.py
@@ -2,10 +2,12 @@ import rasterio as rio
 from PIL import Image
 import numpy as np
 import zipfile
+import imageio
 import shutil
+import cv2
 import os
 
-from .utils import *
+# from .utils import *
 
 def save_pngs(shapeID, iso, base_dir, export_dir = None, v = True, l = 8):
     
@@ -24,42 +26,52 @@ def save_pngs(shapeID, iso, base_dir, export_dir = None, v = True, l = 8):
 
     for zipfolder in os.listdir(FULL_DIR):
     
-        try:
+#         try:
             
-            if v:
-                print("Image ", str(num), " of ", str(dir_length), "---- Month: May")
-                
-            num += 1
+        if v:
+            print("Image ", str(num), " of ", str(dir_length), "---- Month: May")
 
-            image_name = zipfolder.split(".zip")[0]
+        num += 1
 
-            # Extract the RGB TIFF files into the temporary directory
-            with zipfile.ZipFile(os.path.join(FULL_DIR, zipfolder), 'r') as zip_ref:
-                zip_ref.extractall(TEMP_DIR)
+        image_name = zipfolder.split(".zip")[0]
 
-            tiff_files = os.listdir(TEMP_DIR)
+        # Extract the RGB TIFF files into the temporary directory
+        with zipfile.ZipFile(os.path.join(FULL_DIR, zipfolder), 'r') as zip_ref:
+            zip_ref.extractall(TEMP_DIR)
+
+        tiff_files = os.listdir(TEMP_DIR)
+
+        if l == 8:
+            b1 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B2.tif")][0])
+            b2 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B3.tif")][0])
+            b3 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B4.tif")][0])
+
+        elif l == 5:
+            b1 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B1.tif")][0])
+            b2 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B2.tif")][0])
+            b3 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B3.tif")][0])    
             
-            if l == 8:
-                b1 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B2.tif")][0])
-                b2 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B3.tif")][0])
-                b3 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B4.tif")][0])
-                
-            if l == 5:
-                b1 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B1.tif")][0])
-                b2 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B2.tif")][0])
-                b3 = os.path.join(TEMP_DIR, [i for i in tiff_files if i.endswith("B3.tif")][0])                
-
+        
+        if l == 8:
+            
+            prnt("BALSH")
+            
             b1, b2, b3 = rio.open(b1).read(1), rio.open(b2).read(1), rio.open(b3).read(1)
             b1, b2, b3 = Image.fromarray(np.uint16(b1) / 255), Image.fromarray(np.uint16(b2) / 255), Image.fromarray(np.uint16(b3) / 255)
             stack = np.dstack([np.array(i) for i in [b3, b2, b1]])
             PIL_image = Image.fromarray(np.uint8(stack))#.convert('RGB')
 
             PIL_image.save(os.path.join(SAVE_DIR, (image_name + ".png")))
+            
+        elif l == 5:
+            
+            print("L5")
+            
+            b1, b2, b3 = rio.open(b1).read(1), rio.open(b2).read(1), rio.open(b3).read(1)
+            stack = np.dstack([np.array(i) for i in [b3, b2, b1]])
+            print("HERE: ", os.path.join(SAVE_DIR, (image_name + ".png")))
+            imageio.imwrite(os.path.join(SAVE_DIR, (image_name + ".png")), stack)    
 
-            [os.remove(os.path.join(TEMP_DIR, i)) for i in os.listdir(TEMP_DIR)]
-            
-        except Exception as e:
-            
-            print(e)
+        [os.remove(os.path.join(TEMP_DIR, i)) for i in os.listdir(TEMP_DIR)]
 
     shutil.rmtree(TEMP_DIR, ignore_errors = True)
